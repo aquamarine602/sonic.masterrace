8a8e6fd1e986e9d1ca95ba153b14fbb614a24cc7
chloe.spilker <chloe.spilker@CLC-LAB209-04.TCSD.tooelesd.org>
2018-09-04 13:31:37 -0600

    but will it work?

---
+++ src/c/spik3_sonic/masterrace/Sonic_Masterrace.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package c.spik3_sonic.masterrace;
+
+/**
+ * @author chloe.spilker
+ *
+ */
+public class Sonic_Masterrace {
+
+	/**
+	 * 
+	 */
+	public Sonic_Masterrace() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("sonic is masterrace");
+
+	}
+
+}
